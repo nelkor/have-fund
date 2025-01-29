e005081f3a62d8883299c2b9194db47342c377a0
Artem <nelkor@vk.com>
2025-01-29 18:20:31 +0300

    fix style (#4)

--- src/Fund.sol
+++ src/Fund.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 interface IDollar is IERC20 {
     function decimals() external view returns (uint8);
